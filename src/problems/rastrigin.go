aab6dab6f5cc9d6d2937c7ab8d696e282f915e48
megabombus-rex <czechumennakompie@gmail.com>
2025-06-28 19:02:52 +0200

    added rastrigin

---
+++ src/problems/rastrigin.go
@@ -0,0 +1,13 @@
+package rastrigin
+
+import "math"
+
+func rastrigin_fitness(dimensions int, position []float64) float64 {
+	value := float64(10 * dimensions)
+
+	for i := range dimensions {
+		value += (position[i]*position[i] - 10*math.Cos(2*position[i]*math.Pi))
+	}
+
+	return value
+}
